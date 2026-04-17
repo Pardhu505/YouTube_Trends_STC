766c46344975d6af00920213270e3dcbf030c15f
Pardhu505 <168645593+Pardhu505@users.noreply.github.com>
2026-04-17 12:23:11 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -478,4 +478,4 @@ app.add_middleware(
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8001))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=True)
